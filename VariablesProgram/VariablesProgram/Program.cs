2e335f76f04dfeaa228b4d086e9305b0464c7fc1
Alexzander Avila <alexzanderburden@gmail.com>
2025-12-26 15:09:21 -0800

    Completed Second Assignment
    The Variables Program

---
+++ VariablesProgram/VariablesProgram/Program.cs
@@ -0,0 +1,15 @@
+﻿var number = 2; //byte
+var count = 10; //integer
+var totalPrice = 20.95f; //float
+var character = 'A'; //character
+var firstName = "Alex"; //string
+var isWorking = true; //bool
+
+Console.WriteLine(number);
+Console.WriteLine(count);
+Console.WriteLine(totalPrice);
+Console.WriteLine(character);
+Console.WriteLine(firstName);
+Console.WriteLine(isWorking);
+
+Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
