3e93b39b863dc06e3b861f331996579eedd98989
Alexzander Avila <alexzanderburden@gmail.com>
2025-12-26 15:36:02 -0800

    completed fourth assignment
    Operators

---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,20 @@
+﻿//arithmetic
+var a = 10;
+var b = 3;
+Console.WriteLine(a + b); // 13
+Console.WriteLine((float) a / (float) b); // 3.3333~
+
+a = 1;
+b = 2;
+var c = 3;
+Console.WriteLine(a + b * c); // 7
+Console.WriteLine(a > b); // false
+Console.WriteLine(a == b); // false
+Console.WriteLine(a != b); // true
+
+Console.WriteLine(!(a != b)); // false
+
+//logical
+Console.WriteLine(c > b && c > a); // true
+Console.WriteLine(c > b && c ==  a); // false
+Console.WriteLine(c > b || c == a); // true
