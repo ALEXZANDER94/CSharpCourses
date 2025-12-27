869f80e3446d0fa03452d65cf3334e2f0b6c7c19
Alexzander Avila <alexzanderburden@gmail.com>
2025-12-26 18:35:00 -0800

    completed fifth assignment
    Classes

---
+++ Classes/Classes/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace Classes
+{
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("My name is {0}", (FirstName + " " + LastName));
+        }
+    }
+}
---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,19 @@
+﻿using Classes.Math;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
+
+            Calculator calculator = new Calculator();
+            int result = calculator.Add(1, 2);
+            Console.WriteLine("1 + 2 = {0}", result);
+        }
+    }
+}
