0f516aa60d81a1c45604afc4065fd8260b187e2c
Alexzander Avila <alexzanderburden@gmail.com>
2026-01-01 22:52:43 -0800

    Completed SummarizeTextExercise
    a program that implements a string utility class that provides a method for summarizing text

---
+++ SummarizingTextExercise/SummarizingTextExercise/StringUtility.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SummarizingTextExercise
+{
+    public class StringUtility
+    {
+        public static string SummarizeText(string text, int maxLength = 20)
+        {
+            if (text.Length < maxLength)
+            {
+                return text;
+            }
+
+            string[] words = text.Split(' ');
+            int totalCharacters = 0;
+            var summaryWords = new List<string>();
+            foreach (string word in words)
+            {
+                summaryWords.Add(word);
+                totalCharacters += word.Length + 1; // add 1 for the space
+
+                if (totalCharacters > maxLength)
+                {
+                    break;
+                }
+            }
+
+            return String.Join(" ", summaryWords) + "...";
+        }
+    }
+}
