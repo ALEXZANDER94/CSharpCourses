de01a880f87849a1030c88dd161d5190b261b086
Alexzander Avila <alexzanderburden@gmail.com>
2026-01-04 14:59:04 -0800

    Completed Inheritance Exercise
    Created a Stack Class that accepts values of all types using the object keyword and implemented operations for managing the stack, also setup defensive programming within the operations.

---
+++ InheritanceExercises/InheritanceExercises/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+namespace InheritanceExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] validChoices = new string[] { "0", "1", "2", "3" };
+            // 1) A Stack is a data structure for storing a list of elements in a LIFO (last in, first out) fashion.
+            // Design a class called Stack with three methods: Push, Pop, Clear
+            StringBuilder exercise1Menu = new StringBuilder();
+            Stack exercise1Stack = new Stack();
+            exercise1Menu
+                .Append('=', 50)
+                .AppendLine()
+                .AppendLine("1) Push an item to the Stack.")
+                .AppendLine("2) Pop an item from the Stack.")
+                .AppendLine("3) Clear the Stack.")
+                .AppendLine("0) Exit")
+                .Append('=', 50)
+                .AppendLine();
+            string? exercise1Choice = null;
+            do
+            {
+                if(exercise1Choice == "0")
+                {
+                    break;
+                }
+                Console.WriteLine(exercise1Menu);
+                Console.Write("Please make a choice: ");
+                exercise1Choice = Console.ReadLine();
+
+                switch(exercise1Choice)
+                {
+                    case "0":
+                        Console.Clear();
+                        Console.WriteLine("Goodbye!");
+                        break;
+                    case "1":
+                        Console.Clear();
+                        try
+                        {
+                            Console.WriteLine("What would you like to push to the stack?");
+                            string? exercise1PushValue = Console.ReadLine();
+                            exercise1Stack.Push(exercise1PushValue);
+                        }
+                        catch (InvalidOperationException)
+                        {
+                            Console.WriteLine("Push : Invalid Operation");
+                        }
+                        break;
+                    case "2":
+                        Console.Clear();
+                        try
+                        {
+                            exercise1Stack.Pop();
+                        }
+                        catch (InvalidOperationException)
+                        {
+                            Console.WriteLine("Pop : Invalid Operation");
+                        }
+                        break;
+                    case "3":
+                        Console.Clear();
+                        exercise1Stack.Clear();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Choice!");
+                        break;
+                }
+            } while (exercise1Choice != "0");
+        }
+    }
+}
---
+++ InheritanceExercises/InheritanceExercises/Stack.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+
+namespace InheritanceExercises
+{
+    class Stack
+    {
+        private readonly ArrayList StackList = new ArrayList();
+
+
+        public void Push(object value)
+        {
+            Console.Clear();
+            if(value == null)
+            {
+                throw new InvalidOperationException();
+            }
+            StackList.Add(value);
+            Console.WriteLine(string.Format("`{0}` pushed to the Stack", value));
+        }
+
+        public void Pop()
+        {
+            Console.Clear();
+            if(StackList.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            object poppedValue = StackList[StackList.Count - 1];
+            if(poppedValue != null)
+            {
+                StackList.RemoveAt(StackList.Count - 1);
+                Console.WriteLine(string.Format("`{0}` popped from the Stack", poppedValue));
+            }
+        }
+
+
+        public void Clear()
+        {
+            Console.Clear();
+            StackList.Clear();
+            Console.WriteLine("The Stack has been cleared");
+        }
+
+    }
+}
