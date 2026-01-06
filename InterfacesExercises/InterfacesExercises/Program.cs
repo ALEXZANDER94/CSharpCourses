b48388a1bc3d21f916ebf20b93d5385272227836
Alexzander Avila <alexzanderburden@gmail.com>
2026-01-05 23:14:49 -0800

    Completed Interface Exercises
    Created a workflow engine and workflow activity with an interface for executing activity.

---
+++ InterfacesExercises/InterfacesExercises/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace InterfacesExercises
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            // 1) Design a workflow engine that takes a workflow object and runs it. A workflow is a series of steps or activities.
+            // The workflow engine class should have one method called Run() that takes a workflow, and then iterates over each activity in the workflow and runs it
+            try
+            {
+                WorkflowEngine workflowEngine = new WorkflowEngine();
+                workflowEngine.AddActivity(new WorkflowActivity("Uploading a video"));
+                workflowEngine.AddActivity(new WorkflowActivity("Posting video to frontend"));
+                workflowEngine.AddActivity(new WorkflowActivity("Sending an Email"));
+                workflowEngine.AddActivity(new WorkflowActivity("Moving status to published"));
+                workflowEngine.Run();
+            }
+            catch(ArgumentNullException ex)
+            {
+                Console.WriteLine("WorkflowEngine : Null argument found");
+            }
+        }
+    }
+}
---
+++ InterfacesExercises/InterfacesExercises/WorkflowEngine.cs
@@ -0,0 +1,51 @@
+﻿namespace InterfacesExercises
+{
+    public interface IWorkflowActivity
+    {
+        void Execute();
+    }
+    public class WorkflowActivity : IWorkflowActivity
+    {
+        private readonly string _activity;
+
+        public WorkflowActivity(string activity)
+        {
+            if(activity == null)
+            {
+                throw new ArgumentNullException();
+            }
+            this._activity = activity;
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine(string.Format("Executing {0}", this._activity));
+        }
+    }
+    class WorkflowEngine
+    {
+        private readonly List<WorkflowActivity> _activities = new List<WorkflowActivity>();
+
+
+        public void AddActivity(WorkflowActivity newActivity)
+        {
+            if(newActivity == null)
+            {
+                throw new ArgumentNullException();
+            }
+            _activities.Add(newActivity);
+        }
+
+
+        public void Run()
+        {
+            if (_activities.Count > 0)
+            {
+                foreach(WorkflowActivity activity in this._activities)
+                {
+                    activity.Execute();
+                }
+            }
+        }
+    }
+}
